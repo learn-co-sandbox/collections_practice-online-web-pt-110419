e9998840b3dd96eaa0e6274de1f2f14abed3dd0f
Nikelson Exantus <nikelsone1981@hotmail.com>
2019-12-09 20:30:34 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,23 @@
+def sort_array_asc(int)
+  int.sort
+end 
 
+def sort_array_desc(int)
+  int.sort
+  int.sort {|x, y| y <=> x }
+end 
+  
+  def sort_array_char_count(str)
+    str.sort do |a, b| a.length <=> b.length
+  end 
+  end 
+  
+  def swap_elements(string)
+    string.sort_by do |a| a.length
+  end 
+end
+    def reverse_array(int)
+      int.sort! do |a, b| b <=> -a
+    end 
+  end
+  
